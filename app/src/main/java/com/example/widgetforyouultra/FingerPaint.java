1d4fff15f1bf328279c78a10ccae8d381ad70a51
NMonastero <33333449+NMonastero@users.noreply.github.com>
2019-09-16 10:40:23 -0400

    Starting to add Painting
    Lots of research on how to get paint to function. A lot of this is just figuring out what each function does

---
+++ app/src/main/java/com/example/widgetforyouultra/FingerPaint.java
@@ -0,0 +1,14 @@
+package com.example.widgetforyouultra;
+
+import android.graphics.Path;
+
+public class FingerPaint {
+    public int color;
+    public Path path;
+
+    public FingerPaint(int color, Path path){
+        this.color = color;
+        this.path = path;
+    }
+
+}
--- app/src/main/java/com/example/widgetforyouultra/MySurface.java
+++ app/src/main/java/com/example/widgetforyouultra/MySurface.java
@@ -7,24 +7,64 @@ import android.graphics.Paint.Style;
 import android.graphics.PixelFormat;
 import android.view.SurfaceView;
 import android.view.SurfaceHolder;
+import android.graphics.Bitmap;
+import android.graphics.BlurMaskFilter;
+import android.graphics.EmbossMaskFilter;
+import android.graphics.MaskFilter;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class MySurface extends SurfaceView implements SurfaceHolder.Callback {
     private SurfaceHolder surfaceHolder = null;
     private Paint paint = null;
     private float circleX = 0;
     private float circleY = 0;
+    private static final int TOUCH_TOLERANCE = 4;
+    public static int brushSize = 20;
+    public static final int DEFAULT_BRUSH_COLOR = R.color.red;
+    public static final int DEFAULT_BACKGROUND_COLOR = R.color.white;
+    private ArrayList<FingerPaint> paths = new ArrayList();
+    private int currentColor;
+    private int currentBackground = DEFAULT_BACKGROUND_COLOR;
+    private int strokeWidth;
+    private Canvas canvas;
+    private Bitmap bitmap;
 
     public MySurface(Context context) {
-        super(context);
+        super(context, null);
 
-        surfaceHolder = getHolder();
+        //surfaceHolder = getHolder();
 
-        paint = new Paint();
-        paint.setColor(Color.RED);
+        //paint = new Paint();
+        //paint.setColor(Color.RED);
 
         // this.getHolder().setFormat(PixelFormat.TRANSLUCENT);
         // paint.setStyle(Style.FILL);
     }
+
+    public MySurface(Context context, AttributeSet attrs){
+        super(context, attrs);
+        paint.setAntiAlias(true); //smooths out the lines
+        paint.setDither(true); //a little slower but allows for more precise colors
+        //If I was using the default colors I wouldn't want dither but those colors are ugly
+        paint.setColor(getResources().getColor(DEFAULT_BRUSH_COLOR));
+
+        paint.setStyle(Style.STROKE);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setStrokeJoin(Paint.Join.ROUND);//this makes all of the strokes rounded at the end instead of rectangles
+
+        paint.setXfermode(null); //this clears the last path continuity
+        //without it, you would only be able to have one continuous stroke
+
+        paint.setAlpha(0xff);//Some sources say this is necessary as a helper to setColor
+        //However I believe that because I define colors in an xml file, this might not be needed (might remove later)
+    }
+
 
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
